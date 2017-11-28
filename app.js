1d8c409f18da1ab77a044bd177beb1c3ec23854b
paul antezana <yoel.antezana@gmail.com>
2017-11-28 06:57:32 -0500

    post

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var bussines = require('./routes/bussines');
 var desck = require('./routes/desck');
 
 var app = express();
@@ -26,8 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/empresas', bussines);
 app.use('/desk', desck);
+
+require('./routes/bussines')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/bussines.js
+++ routes/bussines.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 
 const { Client } = require('pg');
@@ -7,22 +7,45 @@ const connectionString  = 'postgresql://miruta:miruta@127.0.0.1/miruta';
 
 
 /* GET bussines page. */
-router.get('/all', function(req, res, next) {
-  const client = new Client({
+// router.get('/all', function(req, res, next) {
+//   const client = new Client({
+//     connectionString: connectionString,
+//   });
+//
+//   client.connect();
+//
+//   client.query('SELECT * FROM empresas', (err, result) => {
+//     //console.log(err, response)
+//     if(err != null){
+//       res.json(err);
+//       return;
+//     }
+//     res.json(result.rows)
+//     client.end()
+//   })
+// });
+const client = new Client({
     connectionString: connectionString,
-  });
-
-  client.connect();
-
-  client.query('SELECT * FROM empresas', (err, result) => {
-    //console.log(err, response)
-    if(err != null){
-      res.json(err);
-      return;
-    }
-    res.json(result.rows)
-    client.end()
-  })
 });
 
 module.exports = router;
+module.exports = function (app) {
+    let empresasAll = (req, res)=>{
+        client.connect();
+        client.query('SELECT * FROM empresas', (err, result) => {
+            if(!err){ res.json( result.rows );} else {res.json(err);}
+            client.end();
+        })
+    };
+    let empresasDetail = (req, res)=>{
+      res.setHeader('Content-Type', 'application/json');
+      let currentID = req.body.id || null;
+      client.connect();
+      client.query('SELECT * FROM empresas WHERE id_empresa = ' + currentID, (err, result) => {
+          if(!err){ res.json( result.rows );} else {res.json(err);}
+          client.end();
+      });
+    };
+    app.post('/empresas/detalle',empresasDetail);
+    app.get('/empresas/all',empresasAll);
+};
